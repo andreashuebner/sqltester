38badffc9925010a911a3da0c521a38a7fc0ee32
andreashuebner <andreas.huebnerh@gmail.com>
2015-09-18 21:10:29 +0000

    Switch version to 1.2.1 in development branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='sqltester',
       description = "A python script to generate test queries for tests on data base tables using domain specific language",
-      version='1.2.2',
+      version='1.2.1',
       author='Andras Huebner',
       author_email='andreas.huebnerh@gmail.com',
       url='https://github.com/andreashuebner/sqltester',
